858752f62de82997babe753b0250ce6582f6b10b
Shivam Kumar <shivambspr@gmail.com>
2023-10-08 12:10:33 +0530

    first commit

---
+++ src/main/java/tour_management_system/tms.java
@@ -0,0 +1,48 @@
+package tour_management_system;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class tms extends JFrame implements Runnable {
+	Thread thread;
+	tms(){
+//		setSize(1200, 600);
+//		setLocation(400,200);
+		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/splash.jpg"));
+		Image i2 = i1.getImage().getScaledInstance(1200, 600, Image.SCALE_DEFAULT);
+		ImageIcon i3 = new ImageIcon(i2);
+		JLabel image = new JLabel(i3);
+		add(image);
+		setVisible(true);
+		thread = new Thread(this);
+		thread.start();
+		
+	}
+	public void run() {
+		try {
+			Thread.sleep(7000);
+			setVisible(false);
+		} catch (Exception e) {
+			
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		tms frame = new tms();
+		int x= 1;
+		for(int i=1; i<=500; x+=7, i+=6) {
+			frame.setLocation(750-(x+i)/2, 400-(i/2));
+			frame.setSize(x+i,i);
+			try {
+				Thread.sleep(10);
+			}
+			catch(Exception e) {
+				
+			}
+		}
+		
+	}
+
+}
