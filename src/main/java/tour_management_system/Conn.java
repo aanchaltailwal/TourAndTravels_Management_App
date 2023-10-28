4c4afa8da7bb29c522f42b84daf5c9deaf73f53a
Raghav Mittal <111191629+royalkingzeher@users.noreply.github.com>
2023-10-28 17:20:55 +0530

    Database Connectivity

---
+++ src/main/java/tour_management_system/Conn.java
@@ -0,0 +1,18 @@
+package travel.management.system;
+
+import java.sql.*;
+
+public class Conn {
+  
+        Connection c;
+        Statement s;
+        Conn() {
+        	 try {
+        		 Class.forName("com.mysql.cj.jdbc.Driver");
+        		 c = DriverManager.getConnection("jdbc:mysql:///travelmanagementsystem", "root", "root");
+        	 s = c.createStatement();
+        	 } catch (Exception e) {
+        		 e.printStackTrace();
+        	 }
+        }
+}
