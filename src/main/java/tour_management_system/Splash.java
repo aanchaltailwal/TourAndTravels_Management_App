43ba8d835a35c72a7836ebf80e43976fb52ebabe
aanchaltailwal <112718374+aanchaltailwal@users.noreply.github.com>
2023-11-01 13:39:22 +0530

    Update Splash.java

--- src/main/java/tour_management_system/Splash.java
+++ src/main/java/tour_management_system/Splash.java
@@ -3,9 +3,9 @@ package tour_management_system;
 import javax.swing.*;
 import java.awt.*;
 
-public class tms extends JFrame implements Runnable {
+public class Splash extends JFrame implements Runnable {
 	Thread thread;
-	tms(){
+	Splash(){
 //		setSize(1200, 600);
 //		setLocation(400,200);
 		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/splash.jpg"));
@@ -30,7 +30,7 @@ public class tms extends JFrame implements Runnable {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		tms frame = new tms();
+		Splash frame = new Splash();
 		int x= 1;
 		for(int i=1; i<=500; x+=7, i+=6) {
 			frame.setLocation(750-(x+i)/2, 400-(i/2));
